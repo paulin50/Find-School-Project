6d7ab9a8f0c705e245f8c61bbbcc1c1e19279e52
tiger-githubb <aristidetiger12@gmail.com>
2022-07-29 20:23:28 +0000

    section presentation d'ecole

--- src/components/HomeFeaturedSection.vue
+++ src/components/HomeFeaturedSection.vue
@@ -1,79 +1,291 @@
 <template>
 
-<!-- Start Featured School -->
-<section >
-    <div class="container py-5">
-        <div class="row text-center py-3">
-            <div class="col-lg-6 m-auto">
-                <h1 class="h1">Les Ecoles en Vedette !</h1>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                </p>
+
+    <!-- Start Featured School -->
+    <section>
+        <div class="container py-5">
+            <div class="row text-center py-3">
+                <div class="col-lg-6 m-auto">
+                    <h1 class="h1">Les Ecoles en Vedette !</h1>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab"
+                            data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane"
+                            aria-selected="true">Jardin</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab"
+                            data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane"
+                            aria-selected="false">Primaire</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="contact-tab" data-bs-toggle="tab"
+                            data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane"
+                            aria-selected="false">College</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="contact-tab" data-bs-toggle="tab"
+                            data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane"
+                            aria-selected="false">Lycee</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="contact-tab" data-bs-toggle="tab"
+                            data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane"
+                            aria-selected="false">Université</button>
+                    </li>
+
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
+                        tabindex="0">
+
+                        <div class="row">
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo </a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark"> Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab"
+                        tabindex="0">
+                        <div class="row">
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo </a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark"> Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab"
+                        tabindex="0">
+                        <div class="row">
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo </a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark"> Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab"
+                        tabindex="0">
+                        <div class="row">
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo </a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <a href="#">
+                                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
+                                    </a>
+                                    <div class="card-body">
+                                        <a href="#" class="h2 text-decoration-none text-dark"> Esgis Togo</a>
+                                        <p class="card-text">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui
+                                            officia
+                                            deserunt.
+                                        </p>
+                                        <a href="#" class="btn btn-primary">Lire Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="#">
-                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo </a>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                        </p>
-                        <a href="#" class="btn btn-primary">Lire Plus</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="#">
-                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <a href="#" class="h2 text-decoration-none text-dark">Esgis Togo</a>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                        </p>
-                        <a href="#" class="btn btn-primary">Lire Plus</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="#">
-                        <img src="../assets/img/esgis.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <a href="#" class="h2 text-decoration-none text-dark"> Esgis Togo</a>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                        </p>
-                        <a href="#" class="btn btn-primary">Lire Plus</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  </section>
-  <!-- End Featured School -->
+    </section>
+    <!-- End Featured School -->
 
 </template>
 
 
 <script >
-    export default{
-        name:"HomeFeaturedSection"
-    };
+export default {
+    name: "HomeFeaturedSection"
+};
 </script>
 
 <style scoped>
-
-
-
-.btn-primary{
-    background-color:#006663 !important;
+.btn-primary {
+    background-color: #006663 !important;
     border-color: #006663;
+}
+
+.nav-tabs {
+    margin-bottom: 50px;
+}
+
+.nav-link.active {
+    
 }
 </style>
